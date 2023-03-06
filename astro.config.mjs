66d3f69f1754f78a2bc6ad3742abf97a3a7fe943
yurika1202 <yrpn120293@gmail.com>
2023-03-06 22:40:51 +0900

    add:もくじ作成のため、rehypeのプラグインを追加 #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import rehypeSlug from 'rehype-slug';
+import rehypeToc from 'rehype-toc';
 
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [rehypeSlug, [rehypeToc, { headings: ['h2', 'h3'], nav: true }]],
+  },
   integrations: [
     mdx({
       syntaxHighlight: 'shiki',
@@ -10,7 +15,7 @@ export default defineConfig({
         langs: ['js', 'ts', 'html', 'css', 'scss', 'astro', 'mdx', 'shellscript'],
         wrap: false,
       },
-      draft: true,
+      extendDefaultPlugins: true,
     }),
   ],
 });
