78a6d6ee64fd79ed9b741fdb9e4589ab19facc13
yurika1202 <yrpn120293@gmail.com>
2023-03-11 03:35:27 +0900

    add:cloudflareの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,29 @@ import rehypeSlug from 'rehype-slug';
 import rehypeToc from 'rehype-toc';
 import rehypeExternalLinks from 'rehype-external-links';
 import remarkCodeTitle from 'remark-code-title';
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypeToc, { headings: ['h2', 'h3'], nav: true }],
-      [rehypeExternalLinks, { target: '_blank', rel: 'nofollow' }],
-    ],
-    remarkPlugins: [remarkCodeTitle],
+    rehypePlugins: [rehypeSlug, [rehypeToc, {
+      headings: ['h2', 'h3'],
+      nav: true
+    }], [rehypeExternalLinks, {
+      target: '_blank',
+      rel: 'nofollow'
+    }]],
+    remarkPlugins: [remarkCodeTitle]
   },
-  integrations: [
-    mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: {
-        theme: 'dark-plus',
-        langs: ['js', 'ts', 'html', 'css', 'scss', 'astro', 'mdx', 'shellscript'],
-        wrap: false,
-      },
-      extendDefaultPlugins: true,
-    }),
-  ],
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dark-plus',
+      langs: ['js', 'ts', 'html', 'css', 'scss', 'astro', 'mdx', 'shellscript'],
+      wrap: false
+    },
+    extendDefaultPlugins: true
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
